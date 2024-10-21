dc92ca31cac9a7926edff48c602ec1c753b99abd
Mar Joseph M. Bijer <70209257+Zethkiel@users.noreply.github.com>
2024-10-21 00:50:10 +0000

    Added a function to progressively darken each square

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const container = document.querySelector(".container");
 const resizeButton = document.querySelector(".change-grid");
 let gridSize = 16;
 
+function getRandomColor() {
+    let r = Math.floor(Math.random() * 256); // Random between 0-255
+    let g = Math.floor(Math.random() * 256); // Random between 0-255
+    let b = Math.floor(Math.random() * 256); // Random between 0-255
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+
 function createGrid(size) {
     container.innerHTML = "";
 
     const squareSize = 640 / size;
 
     for (let i = 0; i < size * size; i++) {
+        let brightness = 100;
         const square = document.createElement("div");
         square.classList.add("square");
         square.style.width = `${squareSize}px`;
@@ -15,6 +23,10 @@ function createGrid(size) {
     
         square.addEventListener("mouseenter", () => {
             square.classList.add("active");
+            brightness = brightness - 10;
+
+            square.style.backgroundColor = getRandomColor();
+            square.style.filter = `brightness(${brightness}%)`;
         })
 
         container.appendChild(square);
